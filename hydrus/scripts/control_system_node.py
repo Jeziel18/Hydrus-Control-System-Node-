bb0e0f57100d0dc581eb7aaa98a2a722b5dd8d50
Jeziel E Torres Vázquez <60453228+Jeziel18@users.noreply.github.com>
2022-01-05 12:03:26 -0400

    Add files via upload

--- hydrus/scripts/control_system_node.py
+++ hydrus/scripts/control_system_node.py
@@ -7,23 +7,25 @@ from hydrus.msg import ControlSystemMsg
 
 class ControlsSystemNode:
     def __init__(self):
-        pub = rospy.Publisher('hydrus_custom_msg', ControlSystemMsg, queue_size = 10)
+        
         hydrusMsg = ControlSystemMsg()
+        pub = rospy.Publisher('hydrus_custom_msg', ControlSystemMsg, queue_size = 10)
         self.angular_velocity = rospy.Subscriber('/rexrov2/imu', Imu, self.callback_angular_velocity)
         self.velocity = rospy.Subscriber('/rexrov2/dvl', DVL, self.callback_velocity)
         self.fluid_pressure = rospy.Subscriber('/rexrov2/fluid_pressure', FluidPressure, self.callback_fluid_pressure)
 
     def callback_angular_velocity(self, msg):
-        hydrusMsg.angular_velocity = msg.angular_velocity
-
+        self.hydrusMsg.angular_velocity = msg.angular_velocity
+        pub.publish(hydrusMsg.angular_velocity)
 
     def callback_velocity(self, msg):
-        hydrusMsg.velocity = msg.velocity
+        self.hydrusMsg.velocity = msg.velocity
+        pub.publish(hydrusMsg.velocity)
 
     def callback_fluid_pressure(self, msg):
-        hydrusMsg.fluid_pressure = msg.fluid_pressure
+        self.hydrusMsg.fluid_pressure = msg.fluid_pressure
+        pub.publish(hydrusMsg.fluid_pressure)
 
-    pub.publish(hydrusMsg)
 
 if __name__ == "__main__":
     rospy.init.node('controls_systems-node', anonymous = True)
