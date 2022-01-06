733e63f4170f3f432c6ceb86599552006654e00c
Jeziel E Torres Vázquez <60453228+Jeziel18@users.noreply.github.com>
2022-01-06 01:34:16 -0400

    Add files via upload

--- hydrus/scripts/control_system_node.py
+++ hydrus/scripts/control_system_node.py
@@ -13,21 +13,21 @@ class ControlsSystemNode:
         self.angular_velocity = rospy.Subscriber('/rexrov2/imu', Imu, self.callback_angular_velocity)
         self.velocity = rospy.Subscriber('/rexrov2/dvl', DVL, self.callback_velocity)
         self.fluid_pressure = rospy.Subscriber('/rexrov2/fluid_pressure', FluidPressure, self.callback_fluid_pressure)
-
+        
     def callback_angular_velocity(self, msg):
         self.hydrusMsg.angular_velocity = msg.angular_velocity
-        pub.publish(hydrusMsg.angular_velocity)
+        self.pub.publish(self.hydrusMsg)
 
     def callback_velocity(self, msg):
         self.hydrusMsg.velocity = msg.velocity
-        pub.publish(hydrusMsg.velocity)
+        self.pub.publish(self.hydrusMsg)
 
     def callback_fluid_pressure(self, msg):
         self.hydrusMsg.fluid_pressure = msg.fluid_pressure
-        pub.publish(hydrusMsg.fluid_pressure)
+        self.pub.publish(self.hydrusMsg)
 
 
 if __name__ == "__main__":
-    rospy.init.node('controls_systems-node', anonymous = True)
+    rospy.init_node('controls_systems-node', anonymous = True)
     ControlsSystemNode = ControlsSystemNode()
     rospy.spin()
